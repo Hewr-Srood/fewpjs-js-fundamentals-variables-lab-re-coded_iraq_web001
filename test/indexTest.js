36cb1defcca1114ce848236625d868feaa5023b6
Hewr Srood <hewr_s@outlook.com>
2020-05-15 11:16:31 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,7 +4,7 @@ const path = require('path')
 const babel = require('babel-core');
 
 const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8');
-const companyName = 'Scuber';
+let companyName = 'Scuber';
 
 describe('index.js', function () {
   describe('companyName', function () {
